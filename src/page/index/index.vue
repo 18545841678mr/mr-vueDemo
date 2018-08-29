7d71f046a6fcbcf68e1a15d95545193107dc2bd7
weishengfeng <weishengfeng@ofo.com>
2018-08-29 22:14:06 +0800

    [*]init

---
+++ src/page/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="index">
+        <Top />
+    </div>
+</template>
+<script>
+import Top from '@/components/index/top';
+
+export default {
+    components: {
+        Top
+    }
+}
+</script>
