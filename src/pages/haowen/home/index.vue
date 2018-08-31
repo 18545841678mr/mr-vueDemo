bd046cfc97f06daa6370d56a52f85f2fe63d3a45
marui01 <marui01@ofo.com>
2018-08-31 17:48:13 +0800

    style

--- src/pages/haowen/home/index.vue
+++ src/pages/haowen/home/index.vue
@@ -4,7 +4,7 @@
         <div class="search">
 
         </div>
-        <div class="head-swiper">
+        <!-- <div class="head-swiper">
             <swiper :options="swiperOption" ref="mySwiper">
                 <swiper-slide>I'm Slide 1</swiper-slide>
                 <swiper-slide>I'm Slide 2</swiper-slide>
@@ -15,24 +15,26 @@
                 <div class="swiper-button-next" slot="button-next"></div>
                 <div class="swiper-scrollbar"   slot="scrollbar"></div>
             </swiper>
-        </div>
+        </div> -->
         <div class="hotList">
             <div class="hot-art" v-for="(item, index) in hotList" :key="index">
                 <p class="hotTitle">{{ item.title }}</p>
-                <span class="hot">热</span>
-                <span class="read">{{ item.readNum }}阅读</span>
-                <span class="time">{{item.date}}</span>
+                <div class="info">
+                    <span class="hot">热</span>
+                    <span class="read">{{ item.readNum }}阅读</span>
+                    <span class="time">{{ item.date }}</span>
+                </div>
             </div>
         </div>
         <div class="articleList">
             <div class="artiList" v-for="(item, index) in artiList" :key="index" @click="showDetail">
                 <div class="titleBar">
-                    <p class="hotTitle">{{ item.title }}</p>
+                    <p class="arti">{{ item.title }}</p>
                     <span class="read">{{ item.readNum }}阅读</span>
                     <span class="time">{{ item.date }}</span>
                 </div>
                 <div class="pic">
-                    <img src="{{item.img}}" alt="">
+                    <img src="item.img" alt="">
                 </div>
             </div>
         </div>
@@ -45,7 +47,10 @@ export default {
     name: 'home',
     data(){
         return{
-            hotList: [], //热门文章列表数组
+            hotList: [
+                {title:'洗脸的误区都有哪些 这些错误你至今还在犯吗', readNum: '4.5w', date: '2018-06'},
+                {title:'洗脸的误区都有哪些 这些错误你至今还在犯吗', readNum: '4.5w', date: '2018-06'}
+            ], //热门文章列表数组
             artiList: [], //文章列表数组
             swiperOption: {//swiper官网api，链接http://www.swiper.com.cn/api/
                 // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，　　　　　　　假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
@@ -86,6 +91,80 @@ export default {
 @import '../../../style/normalize.scss';
 
 .haowen {
-
+    color: #000;
+    .title{
+        position: relative;
+        padding-left: rem(5);
+        margin-left: rem(15);
+        width: rem(100);
+        height: rem(50);
+        line-height: rem(50);
+        font-size: rem(15);
+        color: orange;
+        font-weight: 900; 
+        background-color: #fff;
+        &::after {
+            content: '';
+            position: absolute;
+            top: rem(-2);
+            left: 0;
+            width: rem(18);
+            height: 100%;
+            background: url(../../../assets/haowen/WechatIMG15.png) no-repeat left center;
+            background-size: rem(18) auto;
+        }
+    }
+    .hotList{
+        padding: rem(15);
+        .hot-art {
+            margin-top: rem(20);
+            padding-bottom: rem(10);
+            border-bottom: 1px solid #F0F0F0;
+            .hotTitle{
+                font-size: rem(20);
+                font-weight: 500;
+                text-align: start;
+            }
+            .info{
+                width: rem(160); 
+                margin-top: rem(8);
+                .hot{
+                    display: inline-block;
+                    border: 1px solid red;
+                    font-size: rem(15);
+                    width: rem(24);
+                    height: rem(24);
+                    line-height: rem(24);
+                    color: red;
+                }
+                .read{
+                    margin-left: rem(8);
+                    color: #8F8F8F;
+                    font-size: rem(14);
+                }
+                .time{
+                    color: #8F8F8F;
+                    font-size: rem(14);
+                    margin-left: rem(10);
+                }
+            }
+        }
+        
+    }
+    .articleList{
+        padding: rem(15);
+        .artiList{
+            margin-top: rem(20);
+            padding-bottom: rem(10);
+            border-bottom: 1px solid #F0F0F0;
+            .titleBar{
+                .artiTitle{
+                    font-size: rem(20);
+                    font-weight: 500;
+                    text-align: start;
+                }
+            }
+        }
+    }
 }
 </style>
